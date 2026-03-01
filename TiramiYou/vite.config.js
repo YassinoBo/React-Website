9627f4838c1c11bc6093304532fec139ce4e940e
YassinoBo <yassin.bolahrir@gmail.com>
2026-03-01 21:27:26 +0100

    Update vite.config.js

--- TiramiYou/vite.config.js
+++ TiramiYou/vite.config.js
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/React-Website/",
+  plugins: [react(), tailwindcss()],
 })
